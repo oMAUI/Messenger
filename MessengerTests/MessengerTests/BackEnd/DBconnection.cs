8478058c778c569723c322cf059641ebdfc7f0ba
mora <gopnik0770@mail.ru>
2021-06-29 13:53:49 +0300

    Добавлен класс соединения с бд (postgresql)

---
+++ MessengerTests/MessengerTests/BackEnd/DBconnection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Npgsql;
+using System.Data;
+
+namespace TestC.BackEndClass
+{
+    public class DBconnection
+    {
+        private NpgsqlConnection nc;
+
+        public string ConectionString { get; private set; }
+
+
+        public DBconnection(string connectionString)
+        {
+            ConectionString = connectionString;
+            nc = new NpgsqlConnection(ConectionString);
+        }
+
+        public void Open()
+        {
+            nc.Open();
+
+            if (nc.FullState == ConnectionState.Broken || nc.FullState == ConnectionState.Closed)
+            {
+                Console.WriteLine("crash...");
+                Console.Read();
+            }
+
+            Console.WriteLine("OK!");
+        }
+    }
+}
